c2f3957e63912285c9cb443a77cd49b0244af814
joki <jonas.kiefer@live.com>
2023-02-24 11:46:00 +0100

    adds pagination links to schema adds test sceleton

--- drf_spectacular_jsonapi/schemas/pagination.py
+++ drf_spectacular_jsonapi/schemas/pagination.py
@@ -1,5 +1,7 @@
 from rest_framework_json_api.pagination import JsonApiPageNumberPagination
 
+from drf_spectacular_jsonapi.schemas.plumbing import build_pagination_link
+
 
 class JsonApiPageNumberPagination(JsonApiPageNumberPagination):
 
@@ -8,26 +10,19 @@ class JsonApiPageNumberPagination(JsonApiPageNumberPagination):
         return {
             'type': 'object',
             'properties': {
-                # TODO: add json:api link objects for pagination
-                # 'count': {
-                #     'type': 'integer',
-                #     'example': 123,
-                # },
-                # 'next': {
-                #     'type': 'string',
-                #     'nullable': True,
-                #     'format': 'uri',
-                #     'example': 'http://api.example.org/accounts/?{page_query_param}=4'.format(
-                #         page_query_param=self.page_query_param)
-                # },
-                # 'previous': {
-                #     'type': 'string',
-                #     'nullable': True,
-                #     'format': 'uri',
-                #     'example': 'http://api.example.org/accounts/?{page_query_param}=2'.format(
-                #         page_query_param=self.page_query_param)
-                # },
-                'data': schema,
+                'data': {
+                    "type": "array",
+                    "items": schema
+                },
+                "links": {
+                    "type": "object",
+                    "properties": {
+                        "first": build_pagination_link(self),
+                        "last": build_pagination_link(self),
+                        "prev": build_pagination_link(self),
+                        "next": build_pagination_link(self),
+                    }
+                },
             },
-            "required": ["data"]
+            "required": ["data"],
         }
--- drf_spectacular_jsonapi/schemas/plumbing.py
+++ drf_spectacular_jsonapi/schemas/plumbing.py
@@ -1,3 +1,53 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework.fields import empty
+from rest_framework_json_api import serializers
+from rest_framework_json_api.utils import (format_field_name,
+                                           get_related_resource_type,
+                                           get_resource_type_from_serializer)
+
+
+def build_pagination_link(pagination):
+    return {
+        "type": "string",
+        "format": "uri",
+        "nullable": True,  # TODO: only in OpenApi 3.0.x,
+        "example": f"http://api.example.org/resource_path/?{pagination.page_query_param}=4&{pagination.page_size_query_param}=10",
+    }
+
+
+def build_json_api_relationship_object(field):
+    schema = {
+        "type": "object",
+        "properties": {
+            "id": {
+                "type": "string",
+                # TODO: could also provide the correct id format
+                "description": _("The identifier of the related object.")
+            },
+            "type": {
+                "type": "string",
+                "description": _(""),
+                "enum": [get_related_resource_type(field)]
+            }
+        },
+        "required": ["id", "type"],
+    }
+
+    if field.read_only:
+        schema["readOnly"] = True
+    if field.write_only:
+        schema["writeOnly"] = True
+    if field.allow_null:
+        schema["nullable"] = True
+    if field.default and field.default != empty:
+        schema["default"] = field.default
+    if field.help_text:
+        # Ensure django gettext_lazy is rendered correctly
+        schema["description"] = str(field.help_text)
+
+    return schema
+
+
 def build_json_api_data_frame(schema):
     return {
         "type": "object",
